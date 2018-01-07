bb9736e11ce157340c3d5a4fd78f9c11613118c3
sean10 <sean10reborn@gmail.com>
2018-01-08 02:07:35 +0800

    start element-ui

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element, { size: 'small' })
 
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
