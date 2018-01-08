ce794ced8f200765c2e53099b09c726759b6dcff
sean10 <sean10reborn@gmail.com>
2018-01-08 20:35:37 +0800

    little change

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -3,7 +3,7 @@
 	<!-- <div class="line"></div> -->
 	<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
 		<el-menu-item index="1">
-			<router-link to="/todo">Go to Todo</router-link>
+			<router-link to="/todo">TodoList</router-link>
 		</el-menu-item>
 		<!-- <router-link :to="/todo">Todo</router-link> -->
 		<el-submenu index="2">
